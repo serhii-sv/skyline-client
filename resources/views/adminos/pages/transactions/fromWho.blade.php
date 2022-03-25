b725cb4a1b799dfc20027b176e7215c9b184e14b
fladko <vlad.babenko@tewris.com>
2022-03-25 18:23:00 +0200

    fix

--- resources/views/adminos/pages/transactions/fromWho.blade.php
+++ resources/views/adminos/pages/transactions/fromWho.blade.php
@@ -1,6 +1,6 @@
-@if($operation->source)
-    @php $user = \App\Models\User::where('email', $operation->source)->orWhere('id', $operation->source)->first() @endphp
-    @if($user)
-        - {{ $user->login }}
-    @endif
-@endif
+{{--@if($operation->source)--}}
+{{--    @php $user = \App\Models\User::where('email', $operation->source)->orWhere('id', $operation->source)->first() @endphp--}}
+{{--    @if($user)--}}
+{{--        - {{ $user->login }}--}}
+{{--    @endif--}}
+{{--@endif--}}
