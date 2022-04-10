f2c3be992d24b6832477f9ab7c0a1abfce7a84e2
serhii <serhii.voronovych@gmail.com>
2022-04-10 14:26:25 +0300

    update

--- resources/views/adminos/pages/deposits/create.blade.php
+++ resources/views/adminos/pages/deposits/create.blade.php
@@ -272,9 +272,8 @@
                                                                                                         <td>{{ number_format($deposit->balance, $deposit->currency->precision, '.', ',') ?? 0 }} {{ $deposit->currency->symbol }}</td>
                                                                                                         <th scope="col">
                                                                                                             @if($deposit->rate->daily <= 0)
-                                                                                                                @php($perDayPercent = $deposit->overall / $deposit->duration)
-                                                                                                                @php($perDay = $deposit->invested / $deposit->duration)
-                                                                                                                @php($perDay = $perDay + $perDay / 100 * $perDayPercent)
+                                                                                                                @php($perc = ($deposit->overall - 100) / $deposit->duration)
+                                                                                                                @php($perDay = $deposit->invested / 100 * $perc)
                                                                                                                 {{ round(\Carbon\Carbon::parse($deposit->created_at)->diffInDays(now()) * $perDay, $deposit->currency->precision)  }}{{ $deposit->currency->symbol }}
                                                                                                             @else
                                                                                                                 {{number_format($deposit->total_assessed(), $deposit->currency->precision, '.', ',') ?? 0 }} {{ $deposit->currency->symbol }}
